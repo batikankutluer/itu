155c2fdf8abc08c937979ec5c302c763474d6ceb
Batikan-Kutluer <60034174+Batikan-Kutluer@users.noreply.github.com>
2024-11-24 02:09:52 +0300

    ITU Student Tool Website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <></>;
+"use server";
+
+import Image from "next/image";
+
+export default async function Home() {
+  return (
+    <>
+      <div className="w-full h-full flex flex-col justify-evenly items-center bg-zinc-600">
+        <p className="text-6xl font-bold text-zinc-300">İTÜ</p>
+        <div className="flex flex-col justify-center items-center">
+          <Image
+            src={"/images/itu.jpeg"}
+            width={750}
+            height={750}
+            alt="En yakın zamande açılacak, bakımdayız."
+          />
+          <p className="text-zinc-200 p-6 italic">Bakımdayız!</p>
+        </div>
+      </div>
+    </>
+  );
 }
